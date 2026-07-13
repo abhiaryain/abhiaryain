04714f12cc41ee80f98e91d9d7918e20c4349773
Abhishek Kumar <github.abhiarya@gmail.com>
2026-07-13 16:23:13 +0530

    fix: build time env validation

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
+import { validateEnv } from "@/config/server-env";
+
+validateEnv();
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
 };
 
