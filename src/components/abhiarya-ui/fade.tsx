6881275a3e1740141f0b293f22b49f2f0f267428
Abhishek Kumar <github.abhiarya@gmail.com>
2026-07-24 14:40:02 +0530

    test(performance): removed animation

--- src/components/abhiarya-ui/fade.tsx
+++ src/components/abhiarya-ui/fade.tsx
@@ -9,20 +9,20 @@ export function Fade({
 }: ComponentProps<typeof motion.div>) {
   return (
     <motion.div
-      variants={{
-        hidden: {
-          opacity: 0,
-        },
-        show: {
-          opacity: 1,
-          transition: {
-            staggerChildren: 0.07,
-            duration: 0.5,
-          },
-        },
-      }}
-      initial="hidden"
-      animate="show"
+      // variants={{
+      //   hidden: {
+      //     opacity: 0,
+      //   },
+      //   show: {
+      //     opacity: 1,
+      //     transition: {
+      //       staggerChildren: 0.07,
+      //       duration: 0.5,
+      //     },
+      //   },
+      // }}
+      // initial="hidden"
+      // animate="show"
       data-slot="fade"
       // Allows the parent component to pass custom props that override the default motion animation settings.
       {...props}
@@ -38,10 +38,10 @@ export function FadeItem({
 }: ComponentProps<typeof motion.div>) {
   return (
     <motion.div
-      variants={{
-        hidden: { opacity: 0, y: 20, filter: "blur(10px)" },
-        show: { opacity: 1, y: 0, filter: "blur(0px)" },
-      }}
+      // variants={{
+      //   hidden: { opacity: 0, y: 20, filter: "blur(10px)" },
+      //   show: { opacity: 1, y: 0, filter: "blur(0px)" },
+      // }}
       data-slot="fade-item"
       // Allows the parent component to pass custom props that override the default motion animation settings.
       {...props}
